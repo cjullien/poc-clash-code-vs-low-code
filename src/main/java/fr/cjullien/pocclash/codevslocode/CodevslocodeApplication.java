7d243cbba3abb96f5d181917ce70985de6e3792e
cjullien <christophe.JULLIEN@univ-amu.fr>
2022-01-23 00:06:54 +0100

    initialisation spring-boot

---
+++ src/main/java/fr/cjullien/pocclash/codevslocode/CodevslocodeApplication.java
@@ -0,0 +1,13 @@
+package fr.cjullien.pocclash.codevslocode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodevslocodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodevslocodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/cjullien/pocclash/codevslocode/CodevslocodeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cjullien.pocclash.codevslocode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodevslocodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
