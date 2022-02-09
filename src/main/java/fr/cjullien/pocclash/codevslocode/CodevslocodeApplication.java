de85dd2d18e652194242d92c93ba5e221fdf29c7
Christophe JULLIEN <jullienchristophe87@gmail.com>
2022-02-09 21:42:03 +0000

    unsused import

--- src/main/java/fr/cjullien/pocclash/codevslocode/CodevslocodeApplication.java
+++ src/main/java/fr/cjullien/pocclash/codevslocode/CodevslocodeApplication.java
@@ -3,7 +3,6 @@ package fr.cjullien.pocclash.codevslocode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
