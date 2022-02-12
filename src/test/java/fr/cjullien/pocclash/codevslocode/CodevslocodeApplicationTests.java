cdb51d88540b496f6af61e07030231d0e142d614
Christophe JULLIEN <jullienchristophe87@gmail.com>
2022-02-12 20:48:35 +0000

    Ajout tests exemple

--- src/test/java/fr/cjullien/pocclash/codevslocode/CodevslocodeApplicationTests.java
+++ src/test/java/fr/cjullien/pocclash/codevslocode/CodevslocodeApplicationTests.java
@@ -1,13 +1,33 @@
 package fr.cjullien.pocclash.codevslocode;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import fr.cjullien.pocclash.codevslocode.dto.Person;
+import fr.cjullien.pocclash.codevslocode.repository.PersonneRepository;
 
 @SpringBootTest
 class CodevslocodeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@MockBean
+	PersonneRepository personneRepository;
 
+	@Test
+	void emptyRepository() {
+		assertTrue(personneRepository.count() == 0);
+	}
+	
+	@Test
+	void save_nonValidPerson() {
+		try {
+			Person p = new Person();
+			personneRepository.save(p);
+		} catch (Exception e) {
+			fail("Contraintes Violation");
+		}		
+	}
 }
